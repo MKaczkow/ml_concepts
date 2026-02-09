486dd79ff75eac4ee62a216a0fe502472b86c60c
Maciej Kaczkowski <Maciej.Kaczkowski.stud@pw.edu.pl>
2026-02-09 23:32:33 +0100

    FEAT: add LoRA layer implementation for linear models

---
+++ ideas/gpt_from_scratch/lora.py
@@ -0,0 +1,27 @@
+import math
+
+import torch
+import torch.nn as nn
+
+
+class LoRALayer(nn.Module):
+    def __init__(self, in_dim, out_dim, rank, alpha):
+        super().__init__()
+        self.A = nn.Parameter(torch.empty(in_dim, rank))
+        nn.init.kaiming_uniform_(self.A, a=math.sqrt(5))
+        self.B = nn.Parameter(torch.zeros(rank, out_dim))
+        self.alpha = alpha
+
+    def forward(self, x):
+        x = self.alpha * (x @ self.A @ self.B)
+        return x
+
+
+class LinearWithLoRA(nn.Module):
+    def __init__(self, linear, rank, alpha):
+        super().__init__()
+        self.linear = linear
+        self.lora = LoRALayer(linear.in_features, linear.out_features, rank, alpha)
+
+    def forward(self, x):
+        return self.linear(x) + self.lora(x)
